9610066a394c37ce1a705a13fa06f98cad9d81b5
Rick Tesmond <tesmonrd@gmail.com>
2016-03-02 21:50:00 -0800

    tried to make a test but too burned out/too much coding for one day

--- trigrams_test.py
+++ trigrams_test.py
@@ -1,6 +1,11 @@
 import io
 
 
-def test_first():
-    f = io.open('test.txt')
-    
+def test_make_trigram():
+    from trigrams import make_trigram
+    file = io.open(test.txt, encoding='utf-8')
+    assert len(make_trigram(file)) >= len(file)
+
+# def test_make_story():
+#     from trigrams import make_story
+#     assert make_story()
