a872532b3dafd3d9952804853c8f3fb97e4e40b9
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2017-01-10 16:28:46 -0800

    drastically improved speed

--- trigrams.py
+++ trigrams.py
@@ -5,61 +5,40 @@ import sys
 
 def make_trigram(text):
     """Make a dictionary of trigrams."""
-    tri_dict = {}
-    while len(text.split(" ")) > 2:
-        split_words = text.split(" ", 3)
-        our_keys = (split_words[0], split_words[1])
-        our_value = split_words[2]
-        tri_dict.setdefault(our_keys, []).append(our_value)
-        length = len(split_words[0]) + 1
-        text = text[length:]
-    return tri_dict
+    tri = {}
+    text = text.split()
+    {tri.setdefault(tuple(text[i:i + 2]), []).append(text[i + 2]) for i in range(0, len(text) - 2, 3)}
+    return tri
 
 
-def rand_words(dictionary, story):
+def rand_words(dictionary):
     """Get a random key from dictionary and add key/value to story."""
     rand_key = random.choice(dictionary.keys())
-    first = str(rand_key[0])
-    second = str(rand_key[1])
-    third = random.choice(dictionary[rand_key])
-    story.extend([first, second, third])
-    return story
+    return [rand_key[0], rand_key[1], random.choice(dictionary[rand_key])]
 
 
-def make_story(dictionary, num_words):
+def make_story(d, num_words):
     """Use dictionary to create story that is num_words long."""
-    story = []
-    rand_words(dictionary, story)
+    story = rand_words(d)
     while len(story) < num_words:
         last_words = (story[-2], story[-1])
-        if last_words in dictionary:
-            get_lastword = random.choice(dictionary[last_words])
-
-            story.append(get_lastword)
+        if last_words in d:
+            story.append(random.choice(d[last_words]))
         else:
-            rand_words(dictionary, story)
+            story.extend(rand_words(d))
     return " ".join(story)
 
 
 def read_file(fh):
     """Read a file and return the text."""
-    file = io.open(fh, encoding='utf-8')
-    file_text = file.read()
-    file.close()
+    with open(fh, "r") as f:
+        file_text = f.read()
     return file_text
-
-
-def main(path, num_words):
-    """Process make_trigram and make_story to create new story."""
-    text = read_file(path)
-    trigrams = make_trigram(text)
-    random_story = make_story(trigrams, num_words)
-    print random_story
 
 
 def clean_cli(argv):
     if len(argv) != 3:
-        print "You didn't give us a file and a number"
+        print("You didn't give us a file and a number")
         sys.exit(1)
     filename = argv[1]
     num_words = int(argv[2])
@@ -68,4 +47,4 @@ def clean_cli(argv):
 
 if __name__ == "__main__":
     filename, num_words = clean_cli(sys.argv)
-    main(filename, num_words)
+    print(make_story(make_trigram(read_file(filename)), num_words))
--- trigrams_test.py
+++ trigrams_test.py
@@ -6,6 +6,13 @@ def test_make_trigram():
     file = io.open(test.txt, encoding='utf-8')
     assert len(make_trigram(file)) >= len(file)
 
+
+@pytest.mark.parametrize('d, s', ('test.txt', [])
+def test_ran_words_0(d, s):
+        from trigrams import ran_words
+        assert len(ran_words(d, s)) == 3
+
+
 # def test_make_story():
 #     from trigrams import make_story
 #     assert make_story()
